2e1cbef2167e37be08dd6842ea934b85655462d0
Caner Demir <dmrcnr@gmail.com>
2023-06-30 16:40:32 +0300

    value passed to Booklist"

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import BooksContext from "./context/books";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+  <BooksContext.Provider value={5}>
+    <App />
+  </BooksContext.Provider>
+);
